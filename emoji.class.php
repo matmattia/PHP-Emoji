81a46bbc013b18868c261e0d4d7aec4d1fb17956
Mattia <info@matriz.it>
2014-06-26 19:12:01 +0200

    Added emojis & multi Unicode emoji

--- emoji.class.php
+++ emoji.class.php
@@ -4,8 +4,8 @@
  * 
  * @name emoji.class.php
  * @author Mattia - http://www.matriz.it
- * @version 1.0.0
- * @date April 20, 2014
+ * @version 1.0.1
+ * @date June 26, 2014
  * @category PHP Class
  * @copyright (c) 2014 Mattia at Matriz.it (info@matriz.it)
  * @license MIT - http://opensource.org/licenses/mit-license.php
@@ -25,7 +25,77 @@ class Emoji {
 	 * @public
 	 */
 	public function __construct() {
+		// By: http://apps.timwhitlock.info/emoji/tables/unicode
 		$this->unicode = array(
+			'24C2' => 'm',
+			'2702' => 'scissors',
+			'2705' => 'white_check_mark',
+			'2708' => 'airplane',
+			'2709' => 'envelope',
+			'270A' => 'fist',
+			'270B' => 'raised_hand',
+			'270C' => 'v',
+			'270F' => 'pencil2',
+			'2712' => 'black_nib',
+			'2714' => 'heavy_check_mark',
+			'2716' => 'heavy_multiplication_x',
+			'2728' => 'sparkles',
+			'2733' => 'eight_spoked_asterisk',
+			'2734' => 'eight_pointed_black_star',
+			'2744' => 'snowflake',
+			'2747' => '',
+			'274C' => 'x',
+			'274E' => 'negative_squared_cross_mark',
+			'2753' => 'question',
+			'2754' => 'grey_question',
+			'2755' => 'grey_exclamation',
+			'2757' => 'exclamation',
+			'2764' => 'heart',
+			'2795' => 'heavy_plus_sign',
+			'2796' => 'heavy_minus_sign',
+			'2797' => 'heavy_division_sign',
+			'27A1' => 'arrow_right',
+			'27B0' => 'curly_loop',
+			'1F170' => 'a',
+			'1F171' => 'b',
+			'1F17E' => 'o2',
+			'1F17F' => 'parking',
+			'1F18E' => 'ab',
+			'1F191' => 'cl',
+			'1F192' => 'cool',
+			'1F193' => 'free',
+			'1F194' => 'id',
+			'1F195' => 'new',
+			'1F196' => 'ng',
+			'1F197' => 'ok',
+			'1F198' => 'sos',
+			'1F199' => 'up',
+			'1F19A' => 'vs',
+			'1F1E9+1F1EA' => 'de',
+			'1F1EC+1F1E7' => 'gb',
+			'1F1E8+1F1F3' => 'cn',
+			'1F1EF+1F1F5' => 'jp',
+			'1F1F0+1F1F7' => 'kr',
+			'1F1EB+1F1F7' => 'fr',
+			'1F1EA+1F1F8' => 'es',
+			'1F1EE+1F1F9' => 'it',
+			'1F1FA+1F1F8' => 'us',
+			'1F1F7+1F1FA' => 'ru',
+			'1F201' => 'koko',
+			'1F202' => 'sa',
+			'1F21A' => 'u7121',
+			'1F22F' => 'u6307',
+			'1F232' => 'u7981',
+			'1F233' => 'u7a7a',
+			'1F234' => 'u5408',
+			'1F235' => 'u6e80',
+			'1F236' => 'u6709',
+			'1F237' => 'u6708',
+			'1F238' => 'u7533',
+			'1F239' => 'u5272',
+			'1F23A' => 'u55b6',
+			'1F250' => 'ideograph_advantage',
+			'1F251' => 'accept',
 			'1F341' => 'maple_leaf',
 			'1F342' => 'fallen_leaf',
 			'1F383' => 'jack_o_lantern',
@@ -87,6 +157,40 @@ class Emoji {
 			'1F64D' => 'person_frowning',
 			'1F64E' => 'person_with_pouting_face',
 			'1F64F' => 'pray',
+			'1F680' => 'rocket',
+			'1F683' => 'railway_car',
+			'1F684' => 'bullettrain_side',
+			'1F685' => 'bullettrain_front',
+			'1F687' => 'metro',
+			'1F689' => 'station',
+			'1F68C' => 'bus',
+			'1F68F' => 'busstop',
+			'1F691' => 'ambulance',
+			'1F692' => 'fire_engine',
+			'1F693' => 'police_car',
+			'1F695' => 'taxi',
+			'1F697' => 'car',
+			'1F699' => 'blue_car',
+			'1F69A' => 'truck',
+			'1F6A2' => 'ship',
+			'1F6A4' => 'speedboat',
+			'1F6A5' => 'traffic_light',
+			'1F6A7' => 'construction',
+			'1F6A8' => 'rotating_light',
+			'1F6A9' => 'triangular_flag_on_post',
+			'1F6AA' => 'door',
+			'1F6AB' => 'no_entry_sign',
+			'1F6AC' => 'smoking',
+			'1F6AD' => 'no_smoking',
+			'1F6B2' => 'bike',
+			'1F6B6' => 'walking',
+			'1F6B9' => 'mens',
+			'1F6BA' => 'womens',
+			'1F6BB' => 'restroom',
+			'1F6BC' => 'baby_symbol',
+			'1F6BD' => 'toilet',
+			'1F6BE' => 'wc',
+			'1F6C0' => 'bath',
 		);
 	}
 	 
@@ -101,10 +205,17 @@ class Emoji {
 		$html = null;
 		if (is_string($name)) {
 			$name = trim($name, ':');
-			$hex = array_search($name, $this->unicode);
-			if ($hex !== false) {
-				$html = '&#'.hexdec($hex).';';
+			$k = array_search($name, $this->unicode);
+			if ($k !== false) {
+				$html = '';
+				$hex = explode('+', $k);
+				$counter = count($hex);
+				for ($i = 0; $i < $counter; $i++) {
+					$html .= '&#'.hexdec($hex[$i]).';';
+				}
+				unset($i, $counter, $hex);
 			}
+			unset($k);
 		}
 		return $html;
 	}
